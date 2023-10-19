9dbe76b7b7416c1ce7065b79cdf26eeac2e18c1f
seongmin <2018320221@korea.ac.kr>
2023-10-20 02:04:18 +0900

    Implement vae

--- vae/src/main/scala/kuplrg/Implementation.scala
+++ vae/src/main/scala/kuplrg/Implementation.scala
@@ -4,14 +4,46 @@ object Implementation extends Template {
 
   import Expr.*
 
-  def interp(expr: Expr, env: Env): Value = ???
+  def interp(expr: Expr, env: Env): Value = expr match
+    case Num(n)         => n
+    case Add(l, r)      => interp(l, env) + interp(r, env)
+    case Mul(l, r)      => interp(l, env) * interp(r, env)
+    case Val(x, e, b)   => interp(b, env + (x -> interp(e, env)))
+    case Id(x)          => env.getOrElse(x, error(s"free identifier: $x"))
 
-  def freeIds(expr: Expr): Set[String] = ???
+  def freeIds(expr: Expr): Set[String] = expr match
+    case Num(_) => Set()
+    case Add(l, r) => freeIds(l) ++ freeIds(r)
+    case Mul(l, r) => freeIds(l) ++ freeIds(r)
+    case Val(x, e, b) => freeIds(e) ++ (freeIds(b) - x)
+    case Id(x) => Set(x) 
 
-  def bindingIds(expr: Expr): Set[String] = ???
+  def bindingIds(expr: Expr): Set[String] = expr match
+    case Num(_) => Set()
+    case Add(l, r) => bindingIds(l) ++ bindingIds(r)
+    case Mul(l, r) => bindingIds(l) ++ bindingIds(r)
+    case Val(x, e, b) => Set(x) ++ bindingIds(e) ++ bindingIds(b)
+    case Id(_) => Set() 
 
-  def boundIds(expr: Expr): Set[String] = ???
+  def usedIds(expr: Expr): Set[String] = expr match
+    case Num(_)     => Set()
+    case Add(l, r)  => usedIds(l) ++ usedIds(r)
+    case Mul(l, r)  => usedIds(l) ++ usedIds(r)
+    case Val(x, e, b) => usedIds(e) ++ usedIds(b)
+    case Id(x) => Set(x) 
 
-  def shadowedIds(expr: Expr): Set[String] = ???
+  def boundIds(expr: Expr): Set[String] = expr match
+    case Num(_) => Set()
+    case Add(l, r) => boundIds(l) ++ boundIds(r)
+    case Mul(l, r) => boundIds(l) ++ boundIds(r)
+    case Val(x, e, b) => Set(x).intersect(usedIds(b)) ++ boundIds(e) ++ boundIds(b)
+    case Id(x) => Set() 
+
+  def shadowedIds(expr: Expr): Set[String] = expr match
+    case Num(_) => Set()
+    case Add(l, r) => shadowedIds(l) ++ shadowedIds(r)
+    case Mul(l, r) => shadowedIds(l) ++ shadowedIds(r)
+    case Val(x, e, b) => Set(x).intersect(bindingIds(b)) ++ shadowedIds(e) ++ shadowedIds(b)
+    case Id(_) => Set()
 
 }
