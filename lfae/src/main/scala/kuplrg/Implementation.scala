d9476f250ebb8fe1cadd34c11927ac9f984b1745
Seongmin <84278119+ko-success@users.noreply.github.com>
2023-10-23 21:10:25 +0900

    implement lfae

--- lfae/src/main/scala/kuplrg/Implementation.scala
+++ lfae/src/main/scala/kuplrg/Implementation.scala
@@ -5,5 +5,74 @@ object Implementation extends Template {
   import Expr.*
   import Value.*
 
-  def interp(expr: Expr, env: Env): Value = ???
+  def strict(v: Value): Value = v match
+    case ExprV(e, env) => strict(interp(e, env))
+    case _ => v
+
+  type BOp = (BigInt, BigInt) => BigInt
+  def numBOp(x: String)(op: BOp)(l: Value, r: Value): Value = (strict(l), strict(r)) match
+    case (NumV(l), NumV(r)) => NumV(op(l, r))
+    case (l, r) => error(s"invalid operation: ${l.str} $x ${r.str}")
+
+  val numAdd: (Value, Value) => Value = numBOp("+")(_ + _)
+  val numMul: (Value, Value) => Value = numBOp("*")(_ * _)
+
+  def interp(expr: Expr, env: Env): Value = expr match
+    case Num(n)         => NumV(n)
+    case Add(l, r)      => numAdd(interp(l, env), interp(r, env))
+    case Mul(l, r)      => numMul(interp(l, env), interp(r, env))
+    case Id(x)          => env.getOrElse(x, error(s"free identifier: $x"))
+    case Fun(p, b)      => CloV(p, b, env)
+    case App(f, e)      => strict(interp(f, env)) match
+      case CloV(p, b, fenv) => interp(b, fenv + (p -> ExprV(e, env)))
+      case v => error(s"not a function: ${v.str}")
+
+  def strictN(v: Value): Value = v match
+    case ev @ ExprVN(e, env, c) => c match
+      case Some(cache) => cache
+      case None => 
+        val cache = strictN(interpN(e, env)) // 왜 얘는 이렇게 안하지????
+        ExprVN(e, env, Some(cache)) // Expr을 반환하면 상위에서 env 업뎃해주고 내용 가져가는식
+        // val cache = interp(e, env)
+        // ev.value = Some(cache)
+        // cache
+    case _ => v
+
+  def numBOpN(x: String)(op: BOp)(l: Value, r: Value): Value = (l, r) match
+    case (NumV(l), NumV(r)) => NumV(op(l, r))
+    case (l, r) => error(s"invalid operation: ${l.str} $x ${r.str}")
+
+  val numAddN: (Value, Value) => Value = numBOpN("+")(_ + _)
+  val numMulN: (Value, Value) => Value = numBOpN("*")(_ * _)
+
+  def interpN(expr: Expr, env: Env): Value = expr match
+    case Num(n)         => NumV(n)
+    case Add(l, r)      => strictN(interpN(l, env)) match 
+      case ExprVN(e, lenv, Some(c)) =>  // l의 결과가 처음 계산한 exprV이면 r의 env의 exprV을 바꿔줘야함
+        val sl = c
+        val cachedExpr = ExprVN(e, lenv, Some(c))
+        val i = env.find { case (key, value) => value == cachedExpr } // env안에서 참조한 exprV의 id를 역으로 find하는 과정
+        val idOfExpr = i match
+          case Some((key, _)) => key
+          case None => ""
+        val sr = strictN(interpN(r, lenv + (idOfExpr -> cachedExpr))) 
+        numAddN(sl, sr)
+    case Mul(l, r)      => strictN(interpN(l, env)) match 
+      case ExprVN(e, lenv, Some(c)) =>
+        val sl = c
+        val cachedExpr = ExprVN(e, lenv, Some(c))
+        val i = env.find { case (key, value) => value == cachedExpr }
+        val idOfExpr = i match
+          case Some((key, _)) => key
+          case None => ""
+        val sr = strictN(interpN(r, lenv + (idOfExpr -> cachedExpr))) 
+        numMulN(sl, sr)
+    case Id(x)          => env.getOrElse(x, error(s"free identifier: $x"))
+    case Fun(p, b)      => CloV(p, b, env)
+    case App(f, e)      => strictN(interpN(f, env)) match
+      case ExprVN(e, env, Some(c)) => c match
+        case CloV(p, b, fenv) => interpN(b, fenv + (p -> ExprVN(e, env, None)))
+      case CloV(p, b, fenv) => interpN(b, fenv + (p -> ExprVN(e, env, None)))
+      case v => error(s"not a function: ${v.str}")
+
 }
--- lfae/src/test/scala/kuplrg/Spec.scala
+++ lfae/src/test/scala/kuplrg/Spec.scala
@@ -29,4 +29,28 @@ class Spec extends SpecBase {
   testExc(eval("42(1)"), "not a function")
 
   /* Write your own tests */
+
+  // -------------------------------------------------------------------------
+  // interp Call by Need
+  // -------------------------------------------------------------------------
+  test(evalN("1 + 2"), "3")
+  test(evalN("5 * 3"), "15")
+  test(evalN("1 + { val x = 2; x }"), "3")
+  test(evalN("{ val x = 1; x } + 2"), "3")
+  test(evalN("3 * { val x = 2; x }"), "6")
+  test(evalN("{ val x = 5; x } * 5"), "25")
+  testExc(evalN("1 + (x => x)"), "invalid operation")
+  testExc(evalN("(x =>x ) + 2"), "invalid operation")
+  testExc(evalN("1 + { val f = x => x; f }"), "invalid operation")
+  testExc(evalN("{ val f = x => x; f } + 2"), "invalid operation")
+  testExc(evalN("x"), "free identifier")
+  testExc(evalN("x + 2"), "free identifier")
+  test(evalN("x => x"), "<function>")
+  test(evalN("val x = 1 + 2; x"), "<expr>")
+  test(evalN("val x = 1 + 2; x + 3"), "6")
+  test(evalN("val x = 1; val y = 2; x + y"), "3")
+  test(evalN("val f = x => x * 2; val x = 3; f(x)"), "6")
+  test(evalN("val x = 1 + (x => x); 42"), "42")
+  test(evalN("val f = x => 42; f(1 + (x => x)) * 2"), "84")
+  testExc(evalN("42(1)"), "not a function")
 }
